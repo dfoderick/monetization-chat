83a77de5929e60a60d2493d816a445904cc25abb
fullcycle <dfoderick@gmail.com>
2020-05-21 10:52:55 -0700

    Updates

---
+++ precache-manifest.39404a9adce9244e223cd99bda243263.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc621b27e5b7beb2ed7817014addae1e",
+    "url": "/money-chat/index.html"
+  },
+  {
+    "revision": "10f9502ebf099d011d99",
+    "url": "/money-chat/static/css/2.03492def.chunk.css"
+  },
+  {
+    "revision": "e18065219f8ac0752ab8",
+    "url": "/money-chat/static/css/main.232264c9.chunk.css"
+  },
+  {
+    "revision": "10f9502ebf099d011d99",
+    "url": "/money-chat/static/js/2.95897ed5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/money-chat/static/js/2.95897ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18065219f8ac0752ab8",
+    "url": "/money-chat/static/js/main.cbbb38d2.chunk.js"
+  },
+  {
+    "revision": "d0dccd3357bd0bba01ba",
+    "url": "/money-chat/static/js/runtime-main.b2ce6274.js"
+  },
+  {
+    "revision": "315cf35411aa344df92accb26b8ae7b1",
+    "url": "/money-chat/static/media/avatar.315cf354.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/money-chat/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/money-chat/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/money-chat/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/money-chat/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/money-chat/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/money-chat/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/money-chat/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/money-chat/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/money-chat/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/money-chat/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/money-chat/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/money-chat/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/money-chat/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/money-chat/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/money-chat/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/money-chat/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
